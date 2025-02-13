46d75640402f1f167d37b77253fb8b670147ce8c
Mario Hidri <mariohidri2014@yahoo.com>
2025-02-13 20:55:00 +0100

    Added Header, introduction and why me section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { IntroductionComponent } from "./introduction/introduction.component";
+import { WhyMeComponent } from './why-me/why-me.component';
+import { MySkillsComponent } from "./my-skills/my-skills.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [IntroductionComponent, WhyMeComponent, MySkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
