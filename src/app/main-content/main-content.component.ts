8a65c82d2022c5dd99c40ff450f6690016ccc4e8
Mario Hidri <mariohidri2014@yahoo.com>
2025-02-17 23:22:54 +0100

    my projects

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { IntroductionComponent } from "./introduction/introduction.component";
 import { WhyMeComponent } from './why-me/why-me.component';
 import { MySkillsComponent } from "./my-skills/my-skills.component";
+import { MyProjectsComponent } from "./my-projects/my-projects.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [IntroductionComponent, WhyMeComponent, MySkillsComponent],
+  imports: [IntroductionComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
