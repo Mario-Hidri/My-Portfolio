a4c838215ccb5a8999049d14f6b08d6e94ad95ee
Mario Hidri <mariohidri2014@yahoo.com>
2025-02-22 11:36:48 +0100

    contact me section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,11 +3,12 @@ import { IntroductionComponent } from "./introduction/introduction.component";
 import { WhyMeComponent } from './why-me/why-me.component';
 import { MySkillsComponent } from "./my-skills/my-skills.component";
 import { MyProjectsComponent } from "./my-projects/my-projects.component";
+import { ContactFormComponent } from "./contact-me/contact-me.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [IntroductionComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent],
+  imports: [IntroductionComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, ContactFormComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
